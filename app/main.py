7f361da95364876c93d633eda5f566864e331cf1
juliennuellas <julien.nuellas@gmail.com>
2019-05-01 16:51:43 +0200

    some tests had been done

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
+from controller.get_data_from_file import GetData
+
 def main():
     print("hello")
 
 if __name__ == "__main__":
     main()
+    data_file = GetData("./files/test-file.csv")
+    data_file.get_data_into_dict()
